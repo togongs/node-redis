61d6ea8c82979298cda8433407afd4ed5b4fabe9
changwoo Jeong <jcw9828@gmail.com>
2021-12-14 16:09:16 +0900

    설정 완료

--- app.js
+++ app.js
@@ -30,14 +30,29 @@ const redis = new Redis({
   db: 0,
 });
 
-// 숫자는 0부터 시작
-redis.set("number", 0);
-app.get("/", (req, res) => {
-  redis.get("number", (err, number) => {
-    // 현재 숫자를 가져온다
-    redis.set("number", parseInt(number) + 1);
-    res.send("숫자가 1씩 올라갑니다. 숫자: " + number);
-  });
+// set은 key와 value 한 쌍을 저장
+redis.set("number", 8);
+redis.get("number", function (err, data) {
+  console.log(data);
 });
+
+// key value 한 쌍에 여러값
+redis.hmset("codigm", {
+  goormIDE: "cloud service",
+  goormEDU: "edu service",
+});
+redis.hget("codigm", "goormIDE", function (err, value) {
+  // codigm의 해시테이블에서 goormIDE 값 가져오기
+  if (err) throw err;
+  console.log("goormIDE is : " + value); // 해당 값 출력
+});
+
+// app.get("/", (req, res) => {
+//   redis.get("number", (err, number) => {
+//     // 현재 숫자를 가져온다
+//     redis.set("number", parseInt(number) + 1);
+//     res.send("숫자가 1씩 올라갑니다. 숫자: " + number);
+//   });
+// });
 
 module.exports = app;
